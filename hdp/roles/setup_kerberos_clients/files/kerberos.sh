32903ce82153569703ce64e0d26270ef2f993b31
Vikas Srivastava <er.vikassri@gmail.com>
2019-04-10 22:16:33 +0530

    Update kerberos.sh

--- hdp/roles/setup_kerberos_clients/files/kerberos.sh
+++ hdp/roles/setup_kerberos_clients/files/kerberos.sh
@@ -1,3 +1,8 @@
 yum install -y wget unzip
+mkdir /usr/jdk64
+wget http://public-repo-1.hortonworks.com/ARTIFACTS/jdk-8u112-linux-x64.tar.gz
+tar -xvf jdk-8u112-linux-x64.tar.gz
+mv jdk1.8.0_112 /usr/jdk64/
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip"
 unzip -o -j -q jce_policy-8.zip -d  /usr/jdk64/jdk1.8.0_112/jre/lib/security/
+rm -rf jdk-8u112-linux-x64.tar.gz jce_policy-8.zip
